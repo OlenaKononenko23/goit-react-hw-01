c27ff9a941df5aa743851f45892b1b7b02eeb8b6
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-04-29 01:56:30 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
+import userData from "../userData.json";
+import friends from "../friends.json";
+import Profile from "../components/Profile/Profile";
+import FriendList from "../components/FriendList/FriendList";
 
-export default function App() {
-    return (
-        <>
-        <h1>Hello world!</h1>
-        </>
-      
-    )
-}
+export default function App () {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+          />
+          <FriendList friends={friends} />
+    </>
+  );
+};
