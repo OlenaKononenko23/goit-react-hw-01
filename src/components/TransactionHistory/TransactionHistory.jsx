ede3225c2ce95a3bccb16ee6b1168da74f7ee774
AndreyKononenko <167686189+AndreyKononenko@users.noreply.github.com>
2024-04-29 20:56:06 +0300

    1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -12,9 +12,9 @@ export default function TransactionHistory({items}) {
   <tbody>
         {items.map((item) => (
             <tr key={item.id}>
-                <td >{item.type}</td>
+                <td>{item.type}</td>
                 <td>{item.amount}</td>
-                <td >{item.currency}</td>
+                <td>{item.currency}</td>
             </tr>
             
         ))}
